f6ac921b08b63742402c3f3b8a201fd62c343ac4
JuanchoWolf <estefano.chicco@gmail.com>
2022-12-22 16:24:31 -0300

    add Direct Access

--- main.cmd
+++ main.cmd
@@ -1,70 +1,7 @@
 @echo off
 if not "%1" == "max" start /MAX cmd /c %0 max & exit/b
 color 17
-title FixIt
-
-goto check_Permissions
-
-:ini
-cls
-
-echo.
-echo.       ===================================================================================
-echo.       =                                       FIXTOOL                                   =
-echo.       ===================================================================================
-echo.       =                                                                                 =
-echo.       =        1]     Herramientas para Reparar SO                                      =
-echo.       =                                                                                 =
-echo.       =        2]     Herramientas de Internet                                          =
-echo.       =                                                                                 =
-echo.       =        3]     Reparacion Rapida                                                 =
-echo.       =                                                                                 =
-echo.       =        4]     Herramientas de Programas                                         =
-echo.       =                                                                                 =
-echo.       =        5]     Herramientas Adicionales                                          =
-echo.       =                                                                                 =
-echo.       ===================================================================================
-echo.                                             by JuanchoWolf
-echo.           ADVERTENCIA! Para Cerrar el programa pulse el numero 0
-echo.               de lo contrario muchos archivos temporales quedaran en su equipo.
-echo.
-echo.
-echo.
-
-
-set /p tool=Opcion =   
-
-if "%tool%" == "0" goto salir
-if "%tool%" == "1" goto tl1
-if "%tool%" == "2" goto tl2
-if "%tool%" == "3" goto tl3
-if "%tool%" == "4" goto tl4
-if "%tool%" == "5" goto tl5
-
-if not "%tool%" == "0, 1, 2, 3, 4 or 5" goto ini
-
-:tl1
-cd "%~p0\batch"
-repair-so.cmd
-
-:tl2
-cd "%~p0\batch"
-lan.cmd
-
-:tl3
-cd "%~p0\batch"
-flash.cmd
-
-:tl4
-cd "%~p0\batch"
-programs.cmd
-
-:tl5
-cd "%~p0\batch"
-addition.cmd
-
-:salir
-exit
+title FixIt V1.10.2
 
 :check_Permissions
 echo Se requieren permisos administrativos. Detectando permisos...
@@ -77,4 +14,62 @@ if %errorLevel% == 0 (
     echo Fallo: Permisos actuales inadecuados. 
     echo        NECESITA SER ADMINISTRADOR
     pause >nul
+)
+
+:ini
+echo.
+echo.       ===================================================================================
+echo.       =                                      FIXTOOL                                    =
+echo.       ===================================================================================
+echo.       =                                                                                 =
+echo.       =        1]     Herramientas para Reparar SO                                      =
+echo.       =                                                                                 =
+echo.       =        2]     Herramientas de Internet                                          =
+echo.       =                                                                                 =
+echo.       =        3]     Reparacion Rapida                                                 =
+echo.       =                                                                                 =
+echo.       =        4]     Herramientas de Programas                                         =
+echo.       =                                                                                 =
+echo.       =        5]     Herramientas Adicionales                                          =
+echo.       =                                                                                 =
+echo.       =        6]     Accesos Directos                                                  =
+echo.       =                                                                                 =
+echo.       =        0]     Salir                                                             =
+echo.       =                                                                                 =
+echo.       ===================================================================================
+echo.                                          by JuanchoWolf
+echo.
+
+set /p tool=Opcion =   
+
+if "%tool%" == "0" (
+    exit
+)
+if "%tool%" == "1" (
+    cd "%~p0\batch"
+    repair-so.cmd
+)
+if "%tool%" == "2" (
+    cd "%~p0\batch"
+    lan.cmd
+)
+if "%tool%" == "3" (
+    cd "%~p0\batch"
+    flash.cmd
+)
+if "%tool%" == "4" (
+    cd "%~p0\batch"
+    programs.cmd
+)
+if "%tool%" == "5" (
+    cd "%~p0\batch"
+    addition.cmd
+)
+if "%tool%" == "6" (
+    cd "%~p0\batch"
+    access.cmd
+) else (
+    cls
+    echo Opcion Invalida.
+    goto ini
 )
